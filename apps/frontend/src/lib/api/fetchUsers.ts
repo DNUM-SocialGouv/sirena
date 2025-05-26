03e9897e272950504011d52dc2bdd2f709f6f65c
Valère <valere.pique@sg.social.gouv.fr>
2025-05-26 17:36:03 +0200

    fix: fix: remove unused code (#15)

--- apps/frontend/src/lib/api/fetchUsers.ts
+++ apps/frontend/src/lib/api/fetchUsers.ts
@@ -11,7 +11,6 @@
 // }
 
 import { client } from '@/lib/api/hc.ts';
-import type { CreateUserBody } from '@/types/users.d.ts';
 
 export async function fetchUsers() {
   const res = await client.users.$get();
@@ -23,20 +22,6 @@ export async function fetchUsers() {
 export async function fetchUserById(id: string) {
   const res = await client.users[':id'].$get({ param: { id } });
   if (!res.ok) throw new Error('Failed to fetch user');
-  const data = await res.json();
-  return data;
-}
-
-export async function createUser(user: CreateUserBody) {
-  const res = await client.users.$post({ json: user });
-  if (!res.ok) throw new Error('Failed to create user');
-  const data = await res.json();
-  return data;
-}
-
-export async function deleteUser(id: string) {
-  const res = await client.users[':id'].$delete({ param: { id } });
-  if (!res.ok) throw new Error('Failed to delete user');
   const data = await res.json();
   return data;
 }
