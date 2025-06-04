055b66d24e28fbd54d007835bd0cdedc446b4ab4
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-06-04 11:38:48 +0200

    feat: remove dead code

--- apps/backend/src/schemas/apiErrors.schema.ts
+++
@@ -1,7 +0,0 @@
-import { schemas } from '@sirena/backend-utils';
-
-export const ErrorSchema = schemas.ErrorSchema;
-
-export const ZodErrorSchema = schemas.ZodErrorSchema;
-
-export const ZodSafeParseErrorSchema = schemas.ZodSafeParseErrorSchema;
