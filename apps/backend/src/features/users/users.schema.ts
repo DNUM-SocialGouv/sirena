8a46cfa1b6e0b5c19cffa80f8d4a01aee44a1378
Simon Belbeoch <simon.belbeoch@ln1.eu>
2025-06-16 17:13:43 +0200

    refactor: remove dead code

--- apps/backend/src/features/users/users.schema.ts
+++ apps/backend/src/features/users/users.schema.ts
@@ -3,7 +3,7 @@ import { RoleEnumSchema, UserSchema, z } from '@/libs/zod';
 export const UserWithRoleSchema = UserSchema.extend({
   role: RoleEnumSchema.nullable(),
 });
-
+// DEVNOTE: known issue on zod-openapi https://github.com/samchungy/zod-openapi/issues/457
 export const GetUserResponseSchema = UserWithRoleSchema.omit({
   pcData: true,
 });
