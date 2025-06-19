e9e0bed05e959822da52aef479d0c0e542485eed
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-06-19 14:27:15 +0200

    feat: highlight user tab on user/

--- apps/frontend/src/components/layout/sidenav.tsx
+++ apps/frontend/src/components/layout/sidenav.tsx
@@ -1,7 +1,11 @@
 import { useUserStore } from '@/stores/userStore';
 import { SideMenu, type SideMenuProps } from '@codegouvfr/react-dsfr/SideMenu';
-import { useMemo } from 'react';
+import { useMatchRoute } from '@tanstack/react-router';
 
+const useIsOnUserPage = () => {
+  const matchRoute = useMatchRoute();
+  return Boolean(matchRoute({ to: '/users', fuzzy: true }) || matchRoute({ to: '/user/$userId', fuzzy: false }));
+};
 export const SideNav = () => {
   const ADMIN = '/administration' as const;
   const HOME = '/home' as const;
@@ -9,21 +13,19 @@ export const SideNav = () => {
   const ENTITIES = '/entities' as const;
   const USERS = '/users' as const;
 
+  const isOnUserPage = useIsOnUserPage();
   const isAdmin = useUserStore((state) => state.isAdmin);
-  const items = useMemo<SideMenuProps.Item[]>(
-    () => [
-      { linkProps: { to: HOME }, text: 'Accueil' },
-      ...(isAdmin
-        ? [
-            { linkProps: { to: ADMIN }, text: 'Administration' },
-            { linkProps: { to: USERS }, text: 'Gestion des utilisateurs et des habilitations' },
-            { linkProps: { to: ENTITIES }, text: 'Gestion des entités administratives' },
-          ]
-        : []),
-      { linkProps: { to: CASES }, text: 'Gestion des référentiels' },
-    ],
-    [isAdmin, ADMIN, HOME, CASES, ENTITIES, USERS],
-  );
+  const items: SideMenuProps.Item[] = [
+    { linkProps: { to: HOME }, text: 'Accueil' },
+    ...(isAdmin
+      ? [
+          { linkProps: { to: ADMIN }, text: 'Administration' },
+          { isActive: isOnUserPage, linkProps: { to: USERS }, text: 'Gestion des utilisateurs et des habilitations' },
+          { linkProps: { to: ENTITIES }, text: 'Gestion des entités administratives' },
+        ]
+      : []),
+    { linkProps: { to: CASES }, text: 'Gestion des référentiels' },
+  ];
 
   return (
     <>
