34df72e7ade79e51780b799b3a743c058577a8c6
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-06-20 11:50:46 +0200

    feat: create enpoint for getProfile, setup in frontend

---
+++ apps/frontend/src/hooks/queries/useProfile.ts
@@ -0,0 +1,9 @@
+import { fetchProfile } from '@/lib/api/fetcProfile';
+import { useQuery } from '@tanstack/react-query';
+
+export const useProfile = () =>
+  useQuery({
+    queryKey: ['profile'],
+    queryFn: () => fetchProfile(),
+    retry: false,
+  });
