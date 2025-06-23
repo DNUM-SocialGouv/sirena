7f0e577f3f9be3d42f2d1243e2d3928367ab0592
Adrien Chauve <adrien.chauve@sg.social.gouv.fr>
2025-06-23 11:36:14 +0200

    fix: remove cnpg

--- deploy/charts/shared-resources.ts
+++ deploy/charts/shared-resources.ts
@@ -1,32 +1,11 @@
 import { Chart } from 'cdk8s';
 import type { Construct } from 'constructs';
 import * as externalSecrets from '../imports/external-secrets.io';
-import * as cnpg from '../imports/postgresql.cnpg.io';
 
 export class SharedResources extends Chart {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
       disableResourceNameHashes: true,
-    });
-
-    // CNPG PostgreSQL Cluster
-    new cnpg.Cluster(this, 'db', {
-      metadata: {
-        name: 'db',
-      },
-      spec: {
-        bootstrap: {
-          initdb: {
-            database: 'sirena',
-            owner: 'sirena',
-          },
-        },
-        enableSuperuserAccess: true,
-        instances: 1,
-        storage: {
-          size: '1Gi',
-        },
-      },
     });
 
     // Backend External Secret
