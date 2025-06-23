88f20e42a9392ddd3d3e95a656d2649258de9c57
Adrien Chauve <adrien.chauve@sg.social.gouv.fr>
2025-06-23 17:17:45 +0200

    chore: auto deploy (#48)
    * chore: update workflow name
    
    * chore: move workflow to single file
    
    * chore: lint
    
    * chore: add missing synth params
    
    * fix: workflow with IMAGE_TAG
    
    * fix: copy biome config from root config
    
    * chore: add debug
    
    * fix: short sha
    
    * fix: multiple fixes
    
    * fix: add debug line
    
    * fix: fix short sha
    
    * fix: echo
    
    * fix: rm dir
    
    * chore: clean
    
    * chore: update to use new Database resource instead of raw cnpg
    
    * fix: remove cnpg
    
    * chore: use ENVIRONMENT
    
    * chore: add workflows for integration and feature-branches
    
    * chore: factorize github workflows using actions
    
    * fix: build-docker action
    
    * fix: github secret
    
    * chore: factorize with a reusable workflow

--- deploy/charts/shared-resources.ts
+++ deploy/charts/shared-resources.ts
@@ -1,92 +1,105 @@
-import { Chart } from "cdk8s"
-import type { Construct } from "constructs"
-import * as externalSecrets from "../imports/external-secrets.io"
-import * as cnpg from "../imports/postgresql.cnpg.io"
+import { Chart } from 'cdk8s';
+import type { Construct } from 'constructs';
+import * as externalSecrets from '../imports/external-secrets.io';
 
 export class SharedResources extends Chart {
   constructor(scope: Construct, id: string) {
     super(scope, id, {
       disableResourceNameHashes: true,
-    })
-
-    // CNPG PostgreSQL Cluster
-    new cnpg.Cluster(this, "db", {
-      metadata: {
-        name: "db",
-      },
-      spec: {
-        bootstrap: {
-          initdb: {
-            database: "sirena",
-            owner: "sirena",
-          },
-        },
-        enableSuperuserAccess: true,
-        instances: 1,
-        storage: {
-          size: "1Gi",
-        },
-      },
-    })
+    });
 
     // Backend External Secret
-    new externalSecrets.ExternalSecret(this, "backend-external-secret", {
+    new externalSecrets.ExternalSecret(this, 'backend-external-secret', {
       metadata: {
-        name: "backend",
+        name: 'backend',
       },
       spec: {
         dataFrom: [
           {
             extract: {
-              key: "backend",
+              key: 'backend',
             },
           },
         ],
-        refreshInterval: "1h",
+        refreshInterval: '1h',
         secretStoreRef: {
-          name: "local-secret-store",
+          name: 'local-secret-store',
         },
         target: {
-          name: "backend",
+          name: 'backend',
           template: {
             data: {
-              PC_CLIENT_ID: "{{ .PC_CLIENT_ID }}",
-              PC_CLIENT_SECRET: "{{ .PC_CLIENT_SECRET }}",
-              AUTH_TOKEN_SECRET_KEY: "{{ .AUTH_TOKEN_SECRET_KEY }}",
-              REFRESH_TOKEN_SECRET_KEY: "{{ .REFRESH_TOKEN_SECRET_KEY }}",
+              PC_CLIENT_ID: '{{ .PC_CLIENT_ID }}',
+              PC_CLIENT_SECRET: '{{ .PC_CLIENT_SECRET }}',
+              AUTH_TOKEN_SECRET_KEY: '{{ .AUTH_TOKEN_SECRET_KEY }}',
+              REFRESH_TOKEN_SECRET_KEY: '{{ .REFRESH_TOKEN_SECRET_KEY }}',
             },
           },
         },
       },
-    })
+    });
+
+    // Database External Secret
+    new externalSecrets.ExternalSecret(this, 'database-external-secret', {
+      metadata: {
+        name: 'database',
+      },
+      spec: {
+        dataFrom: [
+          {
+            extract: {
+              key: 'databases/sirena',
+            },
+          },
+        ],
+        refreshInterval: '1h',
+        secretStoreRef: {
+          name: 'local-secret-store',
+        },
+        target: {
+          name: 'db',
+          template: {
+            data: {
+              dbname: '{{ .dbname }}',
+              username: '{{ .username }}',
+              password: '{{ .password }}',
+              host: '{{ .host }}',
+              port: '{{ .port }}',
+              url: '{{ .url }}',
+            },
+          },
+        },
+      },
+    });
 
     // GHCR Registry External Secret
-    new externalSecrets.ExternalSecret(this, "ghcr-registry-external-secret", {
+    new externalSecrets.ExternalSecret(this, 'ghcr-registry-external-secret', {
       metadata: {
-        name: "ghcr-registry",
+        name: 'ghcr-registry',
       },
       spec: {
         dataFrom: [
           {
             extract: {
-              key: "ghcr-registry",
+              key: 'ghcr-registry',
             },
           },
         ],
-        refreshInterval: "1h",
+        refreshInterval: '1h',
         secretStoreRef: {
-          name: "local-secret-store",
+          name: 'local-secret-store',
         },
         target: {
-          name: "ghcr-registry",
+          name: 'ghcr-registry',
           template: {
             data: {
-              ".dockerconfigjson": '{"auths":{"{{ .host }}":{"auth":"{{ printf "%s:%s" .username .password | b64enc }}"}}}',
+              '.dockerconfigjson':
+                '{"auths":{"{{ .host }}":{"auth":"{{ printf "%s:%s" .username .password | b64enc }}"}}}',
             },
-            type: "kubernetes.io/dockerconfigjson",
+            type: 'kubernetes.io/dockerconfigjson',
           },
         },
       },
-    })
+    });
   }
-} 
+}
