c4ebcce93ac6d0351ecc038f16794cd6d3ee276b
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-06-30 15:02:45 +0200

    fix: openApi hide pcData

--- apps/backend/src/features/profile/profile.schema.ts
+++ apps/backend/src/features/profile/profile.schema.ts
@@ -2,6 +2,8 @@ import { RoleEnumSchema, UserSchema, z } from '@/libs/zod';
 
 export const ProfileSchema = UserSchema.extend({
   role: RoleEnumSchema.nullable(),
+}).omit({
+  pcData: true,
 });
 
 export const GetProfileResponseSchema = ProfileSchema;
