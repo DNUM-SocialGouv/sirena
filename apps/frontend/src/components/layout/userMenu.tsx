855e9c78867d5c5d948ddf007cd1d1669970a025
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-07-15 11:48:43 +0200

    feat: update versions, add userMenu

--- apps/frontend/src/components/layout/userMenu.tsx
+++ apps/frontend/src/components/layout/userMenu.tsx
@@ -4,10 +4,13 @@ import { useQuery } from '@tanstack/react-query';
 import { useMemo, useState } from 'react';
 import { profileQueryOptions } from '@/hooks/queries/useProfile';
 import './userMenu.css';
+import { ROLES, type Role } from '@sirena/common/constants';
+import { useNavigate } from '@tanstack/react-router';
 
 export const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { data } = useQuery({ ...profileQueryOptions(), enabled: false });
+  const navigate = useNavigate();
 
   const label = useMemo(() => {
     if (!data) {
@@ -16,6 +19,13 @@ export const UserMenu = () => {
     return `${data?.firstName} ${data?.lastName}`;
   }, [data]);
   const email = useMemo(() => data?.email ?? '', [data?.email]);
+  const role = useMemo(() => (data?.role?.id ?? '') as Role | '', [data?.role]);
+
+  const redirectToAdminUsers = () => {
+    navigate({
+      to: '/admin/users',
+    });
+  };
 
   return (
     <Menu.Root onOpenChange={setIsOpen}>
@@ -29,6 +39,15 @@ export const UserMenu = () => {
               {label}
               <span className="fr-hint-text">{email}</span>
             </Menu.Header>
+            <Menu.Separator />
+            {role === ROLES.ENTITY_ADMIN && (
+              <Menu.Item onClick={() => redirectToAdminUsers()}>
+                <div className="user-menu__item">
+                  <span className="fr-icon-user-line user-menu__item__icon" aria-hidden="true" />
+                  Administration
+                </div>
+              </Menu.Item>
+            )}
             <Menu.Separator />
             <Menu.Footer className="user-menu__footer">
               <form action="/api/auth/logout" method="POST">
