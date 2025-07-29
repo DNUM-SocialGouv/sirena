7d5a10f611278b567e191ff3b67c342c8a4d7252
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-07-29 14:52:49 +0200

    feat: adapt db to get dematSocial, prepareFor Cron

--- apps/backend/src/features/requetes/requetes.service.ts
+++ apps/backend/src/features/requetes/requetes.service.ts
@@ -2,6 +2,13 @@ import { RECEPTION_TYPES, REQUETE_STATUT_TYPES } from '@sirena/common/constants'
 import { prisma } from '@/libs/prisma';
 import type { CreateRequeteFromDematSocialDto } from './requetes.type';
 
+export const getRequeteByDematSocialId = async (id: number) =>
+  await prisma.requete.findFirst({
+    where: {
+      dematSocialId: id,
+    },
+  });
+
 export const createRequeteFromDematSocial = async ({ dematSocialId, createdAt }: CreateRequeteFromDematSocialDto) => {
   return await prisma.requete.create({
     data: {
@@ -24,4 +31,14 @@ export const createRequeteFromDematSocial = async ({ dematSocialId, createdAt }:
       },
     },
   });
+};
+
+export const createOrGetFromDematSocial = async ({ dematSocialId, createdAt }: CreateRequeteFromDematSocialDto) => {
+  const requete = await getRequeteByDematSocialId(dematSocialId);
+
+  if (requete) {
+    return null;
+  }
+
+  return await createRequeteFromDematSocial({ dematSocialId, createdAt });
 };
