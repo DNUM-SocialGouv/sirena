02acb2b12a66589c04f136d7760dcb9e7bb940b3
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-09-18 11:49:07 +0200

    feat: update way to get request

--- apps/backend/src/features/requetes/requetes.type.ts
+++ apps/backend/src/features/requetes/requetes.type.ts
@@ -7,11 +7,101 @@ export type CreateRequeteFromDematSocialMinimalDto = {
   commentaire?: string;
 };
 
-export type CreateRequeteFromDematSocialDto = {
-  dematSocialId: number;
-  createdAt: Date;
-  entiteIds: string[];
-  receptionTypeId: string;
-  receptionDate: Date;
+// export type CreateRequeteFromDematSocialDto = {
+//   dematSocialId: number;
+//   createdAt: Date;
+//   entiteIds: string[];
+//   receptionTypeId: string;
+//   receptionDate: Date;
+//   commentaire: string;
+// };
+
+type Declarant = {
+  ageId: string | null;
+  telephone: string | null;
+  estHandicapee: boolean | null;
+  lienVictimeId: string | null;
+  estVictime: boolean;
+  estAnonyme: boolean | null;
+  adresse: {
+    label: string;
+    codePostal: string;
+    ville: string;
+    rue: string;
+    numRue: string;
+  } | null;
+};
+
+type Participant = {
+  telephone: string | null;
+  ageId: string | null;
+  adresse: {
+    label: string;
+    codePostal: string;
+    ville: string;
+    rue: string;
+    numRue: string;
+  } | null;
+  estHandicapee: boolean | null;
+  estVictimeInformee: boolean | null;
+  victimeInformeeCommentaire: string | null;
+  autrePersonnes: string | null;
+} | null;
+
+type LieuDeSurvenue = {
+  codePostal: string;
   commentaire: string;
+  adresse: {
+    label: string;
+    codePostal: string;
+    ville: string;
+    rue: string;
+    numRue: string;
+  } | null;
+  lieuTypeId: string | null;
+  transportTypeId: string | null;
+  societeTransport: string;
+  finess: string;
+};
+
+type MisEnCause = {
+  misEnCauseType: string | null;
+  professionType: string | null;
+  professionDomicileType: string | null;
+  rpps: string | null;
+  commentaire: string | null;
+};
+
+type DemarchesEngagees = {
+  demarches: string[];
+  dateContactEtablissement: Date | null;
+  etablissementARepondu: boolean;
+  organisme: string;
+  datePlainte: Date | null;
+  autoriteTypeId: string | null;
+};
+
+type Fait = {
+  motifs: string[];
+  consequences: string[];
+  maltraitanceTypes: string[];
+  dateDebut: Date | null;
+  dateFin: Date | null;
+  commentaire: string | null;
+};
+
+type Situation = {
+  lieuDeSurvenue: LieuDeSurvenue;
+  misEnCause: MisEnCause;
+  demarchesEngagees: DemarchesEngagees;
+  faits: Fait[];
+};
+
+export type CreateRequeteFromDematSocialDto = {
+  receptionDate: Date;
+  receptionTypeId: string;
+  dematSocialId: number;
+  declarant: Declarant;
+  participant: Participant;
+  situations: Situation[];
 };
