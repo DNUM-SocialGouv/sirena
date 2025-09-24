2624828c768e65d649223bc5870cac6283a0b9b1
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-09-24 17:55:39 +0200

    feat: remove dead code for swagger and dematSocial mapper

--- apps/backend/scripts/updateDematSocial-mapper.ts
+++
@@ -1,5 +0,0 @@
-import { updateObjectPropertyInFile, type Variables } from '@/features/dematSocial/dematSocial.helper';
-
-const variables: Variables[] = [];
-
-updateObjectPropertyInFile('./src/features/dematSocial/dematSocial.mapper.ts', variables);
