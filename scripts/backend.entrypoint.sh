caeb5628285c187c3a0f6155fddbc7ca2c8f1c73
Mickaël <41121479+micktdj@users.noreply.github.com>
2025-09-24 16:16:03 +0200

    fix: fix deploy db reset

--- scripts/backend.entrypoint.sh
+++ scripts/backend.entrypoint.sh
@@ -7,8 +7,5 @@ pnpm --filter @sirena/backend db:deploy
 echo "Init seed"
 pnpm --filter @sirena/backend db:seed
 
-echo "Init db reset"
-pnpm --filter @sirena/backend db:reset
-
 cd /app/apps/backend
 exec node_modules/.bin/tsx src/index.ts
