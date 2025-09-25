9754f61884e712810d560c000b1c600eb5f14cc3
Mickaël <41121479+micktdj@users.noreply.github.com>
2025-09-25 15:22:44 +0200

    fix: fix import

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,7 @@
 export * from './components/Button/Button';
 export * from './components/DataTable/DataTable';
 export { default as Drawer } from './components/Drawer/Drawer';
+export * from './components/InfoSection/InfoSection';
 export * from './components/Loader/Loader';
 export { default as Menu } from './components/Menu/Menu';
 export * from './components/Tabs/Tabs';
