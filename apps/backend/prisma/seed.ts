23e3fe3ec3a6cd89bfa08e75c4644df4b7fe32d5
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-10-14 16:43:06 +0200

    fix: seed with process exit

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -7,30 +7,34 @@ import { seedEntites } from './seed/add_entities';
 import { seedEnums } from './seed/add_enums';
 import { seedRequeteFromDematSocial } from './seed/get_demat_social';
 import '@/libs/instrument';
-
 async function main() {
   const prisma = new PrismaClient();
-
   const logger = createDefaultLogger();
-
   async function seeding() {
-    await seedEnums(prisma).catch((err) => {
+    try {
+      await seedEnums(prisma);
+    } catch (err) {
       logger.error({ err }, '❌ Erreur lors du seeding des enums');
-      process.exit(1);
-    });
-    await seedSuperAdmin(prisma).catch((err) => {
-      logger.error({ err }, '❌ Erreur lors du seeding des super admin:');
-      process.exit(1);
-    });
-    await seedEntites(prisma).catch((err) => {
-      logger.error({ err }, '❌ Erreur lors du seeding des entités:');
-      process.exit(1);
-    });
-    await seedRequeteFromDematSocial().catch((err) => {
+      throw err;
+    }
+    try {
+      await seedSuperAdmin(prisma);
+    } catch (err) {
+      logger.error({ err }, '❌ Erreur lors du seeding du super admin');
+      throw err;
+    }
+    try {
+      await seedEntites(prisma);
+    } catch (err) {
+      logger.error({ err }, '❌ Erreur lors du seeding des entités');
+      throw err;
+    }
+    try {
+      await seedRequeteFromDematSocial();
+    } catch (err) {
       logger.error({ err }, '❌ Erreur lors du seeding des requêtes depuis Demat Social');
-    });
+    }
   }
-
   loggerStorage.run(logger, () => {
     Sentry.withScope((scope) => {
       sentryStorage.run(scope, async () => {
@@ -43,5 +47,4 @@ async function main() {
     });
   });
 }
-
 main();
