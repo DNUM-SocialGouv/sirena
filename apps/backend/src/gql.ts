2a3ae72d6464d1c789d08bc03333f3379ff83ba6
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-10-16 16:39:13 +0200

    wip

--- apps/backend/src/gql.ts
+++ apps/backend/src/gql.ts
@@ -1,11 +1,22 @@
-import { importRequetes } from '@/features/dematSocial/dematSocial.service';
+// import { importRequetes } from '@/features/dematSocial/dematSocial.service';
+
+import { e164 } from 'zod/v4';
+import { getPractionners } from '@/features/esante/esante.service';
 import { createDefaultLogger } from '@/helpers/pino';
 import { loggerStorage } from '@/libs/asyncLocalStorage';
+
+// async function main() {
+//   const logger = createDefaultLogger();
+//   await loggerStorage.run(logger, async () => {
+//     await importRequetes(new Date('2025-09-16'));
+//   });
+// }
 
 async function main() {
   const logger = createDefaultLogger();
   await loggerStorage.run(logger, async () => {
-    await importRequetes(new Date('2025-09-16'));
+    const data = await getPractionners({ ['given:contains']: 'a' });
+    logger.info({ data });
   });
 }
 
