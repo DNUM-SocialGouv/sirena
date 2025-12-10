d0d7ea5880b929a01bae091dd22d8062dc5df408
Valère Pique <valere.pique@sg.social.gouv.fr>
2025-12-10 17:43:55 +0100

    fix: add all motifs

--- apps/frontend/src/components/requestId/requestInfos.tsx
+++ apps/frontend/src/components/requestId/requestInfos.tsx
@@ -5,11 +5,11 @@ import { ContactInfo } from './sections/helpers';
 interface RequestInfosProps {
   requestId?: string;
   fullName: string | null;
-  motif: string | null;
+  motifs: string[];
   statutId: string;
 }
 
-export const RequestInfos = ({ requestId, fullName, motif, statutId }: RequestInfosProps) => {
+export const RequestInfos = ({ requestId, fullName, motifs, statutId }: RequestInfosProps) => {
   // TODO: add more information about the request in header
   return (
     <div className="fr-grid-row fr-grid-row--gutters">
@@ -23,18 +23,18 @@ export const RequestInfos = ({ requestId, fullName, motif, statutId }: RequestIn
             'Nouvelle requête'
           )}
         </h1>
-        {fullName && (
-          <div className={style['legend-display']}>
+        <div className={style['legend-display']}>
+          {!!fullName && (
             <ContactInfo icon="fr-icon-user-line" ariaLabel="Identité">
               {fullName}
             </ContactInfo>
-            {motif && (
-              <ContactInfo icon="fr-icon-todo-line" ariaLabel="Motif de la requête">
-                {motif}
-              </ContactInfo>
-            )}
-          </div>
-        )}
+          )}
+          {motifs.map((motif) => (
+            <ContactInfo key={motif} icon="fr-icon-todo-line" ariaLabel="Motif de la requête">
+              {motif}
+            </ContactInfo>
+          ))}
+        </div>
         {!requestId && (
           <p className="fr-text--sm fr-mb-0">La requête sera créée lorsqu'au moins une donnée sera renseignée</p>
         )}
