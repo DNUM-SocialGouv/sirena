040344e0e7d9aa0bb807d3ea523ff4958a7b20a1
Mickaël <41121479+micktdj@users.noreply.github.com>
2026-02-25 20:02:59 +0100

    feat: seed email field for somes ARS Norm services and directions

---
+++ apps/backend/prisma/migrations/20260225191000_update_ua_emails_ars_normandie/down.sql
@@ -0,0 +1,11 @@
+-- Rollback: set email back to empty for DOS + UA 14, 27, 50, 61, 76
+UPDATE "Entite" SET email = '' WHERE id = '4af829ff-07c1-425d-85d6-83b5f97e4422';
+UPDATE "Entite" SET email = '' WHERE id = '87007621-4f08-4864-8ef1-101a3217691f';
+UPDATE "Entite" SET email = '' WHERE id = 'cc18d397-8c47-4576-aec9-a0f8edcd37a5';
+UPDATE "Entite" SET email = '' WHERE id = 'dc1fe975-28fd-490d-be5b-814a5bdd9c6b';
+UPDATE "Entite" SET email = '' WHERE id = 'c773bd6f-73e8-479c-b552-fd72f91c2efb';
+UPDATE "Entite" SET email = '' WHERE id = '6745d340-b5d9-48b3-878a-72a2498c6a26';
+UPDATE "Entite" SET email = '' WHERE id = '24fafb63-d3db-443b-821d-6de55adc3a0a';
+UPDATE "Entite" SET email = '' WHERE id = 'b9d15159-a71e-4c2f-904b-eb5ebe349d48';
+UPDATE "Entite" SET email = '' WHERE id = '06314126-4d1d-4b19-9127-1f99e0b1bd8b';
+UPDATE "Entite" SET email = '' WHERE id = '1cd16d5a-4967-4d71-a20c-0e68bd7358c1';
---
+++ apps/backend/prisma/migrations/20260225191000_update_ua_emails_ars_normandie/migration.sql
@@ -0,0 +1,10 @@
+UPDATE "Entite" SET email = 'ars-normandie-reclamations-signalements@ars.sante.fr' WHERE id = '4af829ff-07c1-425d-85d6-83b5f97e4422';
+UPDATE "Entite" SET email = 'ars-normandie-reclamations-signalements@ars.sante.fr' WHERE id = '87007621-4f08-4864-8ef1-101a3217691f';
+UPDATE "Entite" SET email = 'ars-normandie-reclamations-signalements@ars.sante.fr' WHERE id = 'cc18d397-8c47-4576-aec9-a0f8edcd37a5';
+UPDATE "Entite" SET email = 'ars-normandie-reclamations-signalements@ars.sante.fr' WHERE id = 'dc1fe975-28fd-490d-be5b-814a5bdd9c6b';
+UPDATE "Entite" SET email = 'ars-normandie-reclamations-signalements@ars.sante.fr' WHERE id = 'c773bd6f-73e8-479c-b552-fd72f91c2efb';
+UPDATE "Entite" SET email = 'ars-normandie-da-reclamations-signalements14@ars.sante.fr' WHERE id = '6745d340-b5d9-48b3-878a-72a2498c6a26';
+UPDATE "Entite" SET email = 'ars-normandie-da-reclamations-signalements27@ars.sante.fr' WHERE id = '24fafb63-d3db-443b-821d-6de55adc3a0a';
+UPDATE "Entite" SET email = 'ars-normandie-da-reclamations-signalements50@ars.sante.fr' WHERE id = 'b9d15159-a71e-4c2f-904b-eb5ebe349d48';
+UPDATE "Entite" SET email = 'ars-normandie-da-reclamations-signalements61@ars.sante.fr' WHERE id = '06314126-4d1d-4b19-9127-1f99e0b1bd8b';
+UPDATE "Entite" SET email = 'ars-normandie-da-reclamations-signalements76@ars.sante.fr' WHERE id = '1cd16d5a-4967-4d71-a20c-0e68bd7358c1';
