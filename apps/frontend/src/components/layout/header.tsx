d35bedc13386ea4ca56a0fc343f8bf707dcdd863
sweetyume <sweet.tsukiyume@gmail.com>
2026-02-27 17:51:47 +0100

    feat(us-480): ajoute liens faq et doc

--- apps/frontend/src/components/layout/header.tsx
+++ apps/frontend/src/components/layout/header.tsx
@@ -9,9 +9,33 @@ type HeaderMenuProps = {
 };
 
 export const HeaderMenu = (props: HeaderMenuProps) => {
-  const userStore = useUserStore();
-  const quickAccessItems = userStore.isLogged ? [<UserMenu key="menu" />] : [];
   const id = useId();
+  const userStore = useUserStore();
+
+  const quickAccessItems = [
+    <a
+      key={'faq'}
+      className="fr-btn fr-btn--tertiary-no-outline fr-btn--sm fr-btn--icon-left fr-icon-arrow-right-line"
+      href="https://docs.numerique.gouv.fr/docs/0221f89c-5118-42b2-8aa1-238e32231c8c/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <abbr title="Foire Aux Questions">FAQ</abbr>
+      <span className="fr-sr-only"> - nouvel onglet</span>
+    </a>,
+    <a
+      key={'doc'}
+      className="fr-btn fr-btn--tertiary-no-outline fr-btn--sm fr-btn--icon-left fr-icon-arrow-right-line"
+      href="https://docs.numerique.gouv.fr/docs/541c745d-7a82-4cbf-b792-c15f69ccf2c7/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Documentation
+      <span className="fr-sr-only"> - nouvel onglet</span>
+    </a>,
+
+    ...(userStore.isLogged ? [<UserMenu key="menu" />] : []),
+  ];
   return (
     <>
       <Header
