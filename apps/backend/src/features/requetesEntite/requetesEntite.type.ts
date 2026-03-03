340ebab115e300e20a04c0c76b765591323c780e
Valère Pique <valere.pique@sg.social.gouv.fr>
2026-03-03 15:27:53 +0100

    feat: add changelog when update requeteStatus

--- apps/backend/src/features/requetesEntite/requetesEntite.type.ts
+++ apps/backend/src/features/requetesEntite/requetesEntite.type.ts
@@ -1,4 +1,10 @@
 import type { z } from 'zod';
+import type { CreateChangeLogDto } from '../changelog/changelog.type.js';
 import type { GetRequetesEntiteQuerySchema } from './requetesEntite.schema.js';
 
 export type GetRequetesEntiteQuery = z.infer<typeof GetRequetesEntiteQuerySchema>;
+
+export type CreateChangeLogForRequeteEntiteDto = Omit<CreateChangeLogDto, 'entity' | 'entityId'> & {
+  requeteId: string;
+  entiteId: string;
+};
