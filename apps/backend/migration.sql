974200b8176b18df3ce2a6e29c638272b7c142ff
Mickaël <41121479+micktdj@users.noreply.github.com>
2026-03-19 15:32:00 +0100

    feat: update entite email/emaildomain

---
+++ apps/backend/migration.sql
@@ -0,0 +1,16 @@
+-- UPDATE
+UPDATE "public"."Entite" SET "email" = 'ARS-NA-MALTRAITANCES@ars.sante.fr', "emailDomain" = 'ars.sante.fr' WHERE "id" = 'f6ad274f-cc6c-4c88-9524-de9a9c9b2127';
+UPDATE "public"."Entite" SET "email" = 'adulte.vulnerable@loire.fr', "emailDomain" = 'loire.fr' WHERE "id" = 'fd550bbd-3faf-4554-bd09-124f392e3581';
+UPDATE "public"."Entite" SET "email" = 'etablissements@puy-de-dome.fr', "emailDomain" = 'puy-de-dome.fr' WHERE "id" = 'b1674ae9-8741-4cf4-aa66-eac3a42b2410';
+UPDATE "public"."Entite" SET "email" = 'dpaph@savoie.fr', "emailDomain" = 'savoie.fr' WHERE "id" = '309b43a6-c3af-4f38-9ad1-1b6831b448ed';
+UPDATE "public"."Entite" SET "email" = 'cellule.paph@doubs.fr', "emailDomain" = 'doubs.fr' WHERE "id" = '307f87f4-c677-4b8b-8eee-d92c0ac1d299';
+UPDATE "public"."Entite" SET "emailDomain" = 'territoiredebelfort.fr' WHERE "id" = '37b48a7d-8ee5-4777-a08f-4c7b9fe1028b';
+UPDATE "public"."Entite" SET "email" = 'isl@ille-et-vilaine.fr', "emailDomain" = 'ille-et-vilaine.fr' WHERE "id" = 'b8e2bee2-e881-4a3f-99c1-5ade6c03c995';
+UPDATE "public"."Entite" SET "email" = 'dpaph@finistere.fr', "emailDomain" = 'finistere.fr' WHERE "id" = 'b08c08b6-cf30-4f0b-9453-66d7233398c6';
+UPDATE "public"."Entite" SET "email" = 'sgo-qualite@morbihan.fr', "emailDomain" = 'morbihan.fr' WHERE "id" = '394c561c-f1d7-4bb0-b8f7-fc5b8d02a0be';
+UPDATE "public"."Entite" SET "email" = 'president@eurelien.fr', "emailDomain" = 'eurelien.fr' WHERE "id" = 'e761c153-193f-43d9-b06e-5b0a3b654115';
+UPDATE "public"."Entite" SET "email" = 'signalementspaph@lacharente.fr', "emailDomain" = 'lacharente.fr' WHERE "id" = '931edf21-3127-482f-95cf-a23a9ab225dc';
+UPDATE "public"."Entite" SET "email" = 'da-esms@charente-maritime.fr', "emailDomain" = 'charente-maritime.fr' WHERE "id" = '95fccf6d-ab7e-426d-969c-78873a5cfcfe';
+UPDATE "public"."Entite" SET "email" = 'adulte-vulnerable@gironde.fr', "emailDomain" = 'gironde.fr' WHERE "id" = '9a42f674-1d36-499f-9895-76a3ef4b06c2';
+UPDATE "public"."Entite" SET "email" = 'eig_paph@departement86.fr', "emailDomain" = 'departement86.fr' WHERE "id" = '2224ba6c-a140-4e74-8556-e242c94fb7d4';
+UPDATE "public"."Entite" SET "email" = 'ars-oc-duajiq-reclamations-usagers@ars.sante.fr', "emailDomain" = 'ars.sante.fr' WHERE "id" = 'e9ed1e3a-3a32-4078-9109-2c83c12c4faf';
