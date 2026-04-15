f6d9c60890661b0779ac8a02ccd07ec60b8158d1
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-04-15 17:12:22 +0200

    chore: use pino for migration logging system

--- apps/migration/src/utils/logs.ts
+++ apps/migration/src/utils/logs.ts
@@ -1,7 +1,47 @@
+import pino from 'pino';
+import pretty from "pino-pretty";
+
+
+export const createPinoConfig = (): pino.LoggerOptions => {
+  return {
+    level: "info",
+    serializers: {
+      err: pino.stdSerializers.err,
+      req: (req: { method: string; url: string }) => ({
+        method: req.method,
+        url: req.url,
+      }),
+      res: (res: { status: number }) => ({
+        status: res.status,
+      }),
+    },
+  };
+};
+
+export const createPrettyConfig = (messageFormat: string): pretty.PrettyStream | undefined =>
+  process.env.LOG_FORMAT === 'pretty'
+    ? pretty({
+      ignore: 'pid,hostname',
+      translateTime: 'SYS:standard',
+      messageFormat,
+    })
+    : undefined;
+
+export const logger =  pino(createPinoConfig(), createPrettyConfig('{msg}'));
+
+
 export const logMessage = (message: string, ...args: unknown[]) => {
-  console.log(message, ...args);
+  if (args.length > 0) {
+    logger.info({ args }, message);
+  } else {
+    logger.info(message);
+  }
 };
 
 export const logError = (message: string, ...args: unknown[]) => {
-  console.error(message, ...args);
+  if (args.length > 0) {
+    logger.error({ args }, message);
+  } else {
+    logger.error(message);
+  }
 };
