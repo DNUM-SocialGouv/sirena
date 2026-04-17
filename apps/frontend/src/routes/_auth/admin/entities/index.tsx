d45da557d3e20a7675788a27cff60296d820361e
Kevin Nguyen <knth@hey.com>
2026-04-17 16:48:55 +0200

    feat: add page title for entites admin page

--- apps/frontend/src/routes/_auth/admin/entities/index.tsx
+++ apps/frontend/src/routes/_auth/admin/entities/index.tsx
@@ -4,6 +4,13 @@ import { requireAuthAndRoles } from '@/lib/auth-guards';
 
 export const Route = createFileRoute('/_auth/admin/entities/')({
   beforeLoad: requireAuthAndRoles([ROLES.SUPER_ADMIN]),
+  head: () => ({
+    meta: [
+      {
+        title: 'Gestion des entités - Espace administrateur -SIRENA',
+      },
+    ],
+  }),
   component: RouteComponent,
 });
 
