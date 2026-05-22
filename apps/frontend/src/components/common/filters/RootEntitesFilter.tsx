e2a515b7340f0c175437d66c6fc7e68dc85e1cc1
Kevin Nguyen <knth@hey.com>
2026-05-22 15:48:23 +0200

    feat: add RootEntitesFilter

---
+++ apps/frontend/src/components/common/filters/RootEntitesFilter.tsx
@@ -0,0 +1,36 @@
+import { useMemo } from 'react';
+import { DropdownCheckboxFilter } from './DropdownCheckboxFilter';
+
+type RootEntite = {
+  id: string;
+  nomComplet: string;
+  label: string;
+};
+
+type Props = {
+  rootEntites: RootEntite[];
+  selectedRootEntiteIds: string[];
+  onChange: (rootEntiteIds: string[]) => void;
+};
+
+export function RootEntitesFilter({ rootEntites, selectedRootEntiteIds, onChange }: Props) {
+  const options = useMemo(
+    () =>
+      rootEntites.map((rootEntite) => ({
+        value: rootEntite.id,
+        label: rootEntite.nomComplet,
+      })),
+    [rootEntites],
+  );
+
+  return (
+    <DropdownCheckboxFilter
+      buttonLabel="Entité administrative"
+      legend="Filtrer les entités par entité administrative"
+      hintText="Entité administrative"
+      options={options}
+      selectedValues={selectedRootEntiteIds}
+      onChange={onChange}
+    />
+  );
+}
