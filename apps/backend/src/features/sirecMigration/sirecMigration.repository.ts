a4c6d5334e20e6ce3ca06ec267d09a6a40ae7149
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-07-02 17:44:23 +0200

    feat: ne pas tenir compte des mis en cause avec l'id group 0

--- apps/backend/src/features/sirecMigration/sirecMigration.repository.ts
+++ apps/backend/src/features/sirecMigration/sirecMigration.repository.ts
@@ -266,7 +266,7 @@ export async function fetchSirecMisEnCauses(sirecId: number): Promise<SirecMisEn
             f.rs AS finess_rs, f.codepostal AS finess_codepostal, f.libcommune AS finess_libcommune,
             f.numvoie AS finess_numvoie, f.typevoie AS finess_typevoie, f.voie AS finess_voie
      FROM sire_misencause_data m
-     LEFT JOIN sire_misencause_data_group mcg ON m.id_data = mcg.id_data AND mcg.id_group != 1
+     LEFT JOIN sire_misencause_data_group mcg ON m.id_data = mcg.id_data AND mcg.id_group != 1 AND mcg.id_group != 0
      LEFT JOIN sire_rpps_data r ON r.id_data = m.identifiant AND m.type = 65
      LEFT JOIN sire_finess_data f ON f.id_data = m.identifiant AND m.type = 64
      WHERE m.id_reclamation = ?`,
