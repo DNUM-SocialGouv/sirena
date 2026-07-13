0c27f6cafb455f3e82e5254668df79bdc3fb117b
Raphaël Taïeb <raphael.taieb.fr@gmail.com>
2026-07-13 18:34:43 +0200

    fix: correction des entités PACA

--- apps/backend/src/features/sirecMigration/transco/affectation/entitesPACA.ts
+++ apps/backend/src/features/sirecMigration/transco/affectation/entitesPACA.ts
@@ -1,15 +1,15 @@
 import type { EntiteSirenaLabels } from './affectation.transco.js';
 
 export const AFFECTATION_ENTITES_PACA: Record<number, [EntiteSirenaLabels, ...EntiteSirenaLabels[]]> = {
-  1027: [{ label: 'DD04', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  1029: [{ label: 'DD05', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  1031: [{ label: 'DD06', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  1033: [{ label: 'DD13', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  1035: [{ label: 'DD83', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  1037: [{ label: 'DD84', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  1039: [{ label: 'DOMS', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  1043: [{ label: 'DOS - Département Pharmacie et Biologie', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  5030: [{ label: 'DSDP', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  5024: [{ label: 'DSPE - EIGS', parentLabel: "Provence-Alpes-Côte d'Azur" }],
-  2297: [{ label: 'DAJI - SICR', parentLabel: "Provence-Alpes-Côte d'Azur" }],
+  1027: [{ label: 'DD04', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  1029: [{ label: 'DD05', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  1031: [{ label: 'DD06', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  1033: [{ label: 'DD13', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  1035: [{ label: 'DD83', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  1037: [{ label: 'DD84', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  1039: [{ label: 'DOMS', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  1043: [{ label: 'DOS - Département Pharmacie et Biologie', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  5030: [{ label: 'DSDP', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  5024: [{ label: 'DSPE - EIGS', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
+  2297: [{ label: 'DAJI - SICR', parentLabel: "ARS Provence-Alpes-Côte d'Azur" }],
 };
